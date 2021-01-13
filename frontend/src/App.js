55a333d23c85bbbee8515b4f6f152f1e826495d4
lucas-barroso-23 <lucas_barroso@atlantico.com.br>
2021-01-13 08:59:22 -0300

    mobile setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import Header from "./components/Header";
+
+import "./App.css";
+import BackgroundImage from "./assets/jordans.jpg";
 
 /**
  * Componente
  * Propiedade
- * estado
+ * estado & imutabilidade
  */
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("projects").then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    //projects.push(`Novo projeto ${Date.now()}`);
+    // setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    const response = await api.post("projects", {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Lucas Barroso",
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+
+    localStorage.setItem("projects", JSON.stringify(projects));
+
+    console.log(projects);
+  }
+
+  const handleDeleteProject = () => {
+    setProjects([]);
+  };
+
+  const handleBackupProject = () => {
+    const projectsBackup = JSON.parse(localStorage.getItem("projects"));
+
+    setProjects(projectsBackup);
+  };
+
   return (
     <>
-      <Header title="Homepage">
-        <ul>
-          <li>lista1</li>
-          <li>lista2</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
-        <ul>
-          <li>lista3</li>
-          <li>lista4</li>
-        </ul>
-      </Header>
+      <Header title="Projects" />
+
+      <img src={BackgroundImage} alt="jordans" />
+
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>{project.title}</li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
+
+      <button type="button" onClick={handleDeleteProject}>
+        Deletar projetos
+      </button>
+
+      <button type="button" onClick={handleBackupProject}>
+        Backup projetos
+      </button>
     </>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,9 @@
 import React from "react";
 
-export default function header({ title, children }) {
+export default function header({ title }) {
   return (
     <header>
       <h1>{title}</h1>
-
-      {children}
     </header>
   );
 }
